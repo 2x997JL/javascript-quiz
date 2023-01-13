b843d98be30d2c3e558be2e35e7502436926835b
Jemilah Leonard <jemilah.leonard4@gmail.com>
2023-01-13 08:08:52 -0600

    added questions

--- assets/questions.js
+++ assets/questions.js
@@ -1,27 +1,27 @@
 var questions = [
     {
-        question:
-        options:
-        answer:
+        question: "Which JavaScript label catches all the values, except for the ones specified?",
+        options: ["Default", "Dogs", "Peter", "Fries"],
+        answer: "Default",
     },
     {
-        question:
-        options:
-        answer  
+        question: "How do you find the minimum of x and y using JavaScript?",
+        options: ["x-y", "Math.min(x,y)", "xxx", "yyy"],
+        answer: "Math.min(x,y)",  
     },
     {
-        question:
-        options:
-        answer:
+        question: "What will the code return? Boolean(3 < 7)",
+        options: ["false", "none", "true", "IDK"],
+        answer: "true",
     },
     {
-        question:
-        options:
-        answer
+        question: "JavaScript is a ___ side programming language",
+        options: ["Server", "Disney", "Client", "The 1975"],
+        answer: "Server",
     },
     {
-        question:
-        options:
-        answer:
+        question: "Which of the following will write the message “Hello Mate!” in an alert box?",
+        options: ["alert(HelloMate!);", "msgAlert('Hell Mate!')", "alert('Hello Mate!');", "none of the above"],
+        answer: "alert('Hello Mate!');",
     }
-]
+];
